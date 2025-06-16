b3d7c3eb40771c01fc43fd2cc2dfbc839eb6e202
Chuchla <jacobchuchlaschooling@gmail.com>
2025-06-16 20:27:34 +0200

    Fix login

--- backend/GymDB/settings.py
+++ backend/GymDB/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'Gym_sys_app',
         'USER': 'root',
-        'PASSWORD': '123456',
+        'PASSWORD': 'Pies1234pies!',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -141,3 +141,45 @@ AUTH_USER_MODEL = 'GymUs.Client'
 # CORS_ORIGIN_WHITELIST = [
 #     'http://localhost:8000'
 # ]
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "VERIFYING_KEY": "",
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "JTI_CLAIM": "jti",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+}
--- frontend/src/components/AxiosInstance.jsx
+++ frontend/src/components/AxiosInstance.jsx
@@ -1,5 +1,6 @@
 // src/api/axiosInstance.jsx
 import axios from "axios";
+import { logout } from "../Utils/Auth.jsx";
 
 const baseUrl = "http://127.0.0.1:8000/api/";
 
@@ -26,6 +27,47 @@ AxiosInstance.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error),
+);
+
+AxiosInstance.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      if (refreshToken) {
+        try {
+          const response = await axios.post(`${baseUrl}token/refresh/`, {
+            refresh: refreshToken,
+          });
+
+          const { access } = response.data;
+
+          localStorage.setItem("accessToken", access);
+
+          AxiosInstance.defaults.headers.common["Authorization"] =
+            `Bearer ${access}`;
+          originalRequest.headers["Authorization"] = `Bearer ${access}`;
+
+          return AxiosInstance(originalRequest);
+        } catch (refreshError) {
+          console.error("Refresh token is invalid or expired", refreshError);
+          logout();
+          return Promise.reject(refreshError);
+        }
+      } else {
+        console.log("No refresh token available, logging out.");
+        logout();
+      }
+    }
+
+    return Promise.reject(error);
+  },
 );
 
 export default AxiosInstance;
